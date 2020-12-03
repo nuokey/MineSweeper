50069ab5d1daf8373b136457fd4c233bd75688b0
Timo <66961498+nuokey@users.noreply.github.com>
2020-12-03 09:24:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from tkinter import *
 
-class Mine():
-	def __init__(self):
-		
-
 window = Tk()
-window.geometry("500x500")
+window.geometry("500x550")
 
 mines = []
-for i in range(10):
-	mines.append(Button())
+for x in range(10):
+	for y in range(10):
+		mine = Button(window, text = "", bg='grey')
+		mine.place(x = x * 50, y = y * 50 + 50, width = 50, height = 50)
 
 window.mainloop()
